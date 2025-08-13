8542803489fd685f6f7f2d3b02b2352a5492f60d
mamadfar <m.sabah.farhadi@gmail.com>
2025-08-13 17:33:12 +0200

    #feat: pipenv, pip, virtual env, publishing a package

---
+++ 5.python-package-index/3.publishing_package/app.py
@@ -0,0 +1,4 @@
+
+from mamadpdf import pdf2text
+
+pdf2text.Convertor().convert_to_text("path/to/your/pdf/file.pdf")
---
+++ 5.python-package-index/3.publishing_package/mamadpdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provides functions to convert a PDF to text."""
+
+
+class Convertor:
+    """ A simple convertor for converting PDF to text. """
+
+    def convert_to_text(self, path: str):
+        """
+        A brief description of what the function does.
+
+        Args:
+            path (str): The path to the PDF file.
+
+        Returns:
+            None: The function does not return a value.
+        """
+        print("Converting PDF to text...")
